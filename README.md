97139d983d89cc43f733a19ae7d46fa22ad8c4ff
Al <mipoem@gmail.com>
2013-10-15 11:18:07 +0800

    reinit

--- README.md
+++
@@ -1,4 +0,0 @@
-osd
-===
-
-This is a learning project to me for use git and programing with linux
--- osd-dev/src/genmarshal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-glib-genmarshal --header --prefix ol_marshal marshal > ol_marshal.h
-glib-genmarshal --body --prefix ol_marshal marshal > ol_marshal.c
-
